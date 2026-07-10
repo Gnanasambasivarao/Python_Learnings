e12535a5aac0557512a8888f194bf335fa62d677
Gnanasambasivarao <thotagnanasambasivarao25@gmail.com>
2026-07-10 18:21:32 +0530

    Day8: Implemented and understood user input.

---
+++ Day8/user_input.py
@@ -0,0 +1,13 @@
+# we use input() for taking input data from user
+# Basically input takes value in string data type.
+user_input=input("Enter a value.")
+print(f"USer_input {user_input}")
+
+# If we have to take input in integer
+
+user1=int(input("Enter a integer value."))
+print(user1)
+
+user2=float(input("Enter float value")) # decimal value
+print(user2)
+
